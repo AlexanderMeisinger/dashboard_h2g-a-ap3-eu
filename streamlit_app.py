b92661097e28603a0be38f8bcdf54a7f43d6335a
AlexanderMeisinger <alexander.meisinger@oth-regensburg.de>
2025-01-21 09:38:50 -0800

    Clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -384,7 +384,7 @@ if (display == "Scenario comparison") and (number_sensitivities <= 1):
     choices = config["scenarios"]
     idx = st.selectbox("View", choices, format_func=lambda x: choices[x], label_visibility='hidden')
 
-    ds = xr.open_dataset("data/scenarios-h2-co2.nc")
+    ds = xr.open_dataset("data/scenarios.nc")
 
     accessors = {k: v for k, v in sel.items() if k not in ['power_grid', 'hydrogen_grid']}
     df = ds[idx].sel(**accessors, drop=True).to_dataframe().squeeze().unstack(level=0).dropna(axis=1)
